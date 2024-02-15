22ff2c409e3d9c0c70afa0d1544abfe61f6c20d7
WIllianMartins2018 <willian.martins01@hotmail.com>
2024-02-15 19:38:53 -0300

    Tipando o KNEX

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -23,4 +23,36 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return replay.status(201).send()
   })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'Amount' })
+      .first()
+
+    return {
+      summary,
+    }
+  })
 }
